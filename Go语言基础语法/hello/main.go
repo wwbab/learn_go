4c0df986c4819b1b201cdc152ce72ce5c5c88151
wuwenbin <3303274053@qq.com>
2025-02-13 17:08:40 +0800

    Hello World

---
+++ Go语言基础语法/hello/main.go
@@ -0,0 +1,9 @@
+package main // 声明 main 包，表明这是一个可执行程序
+
+import (
+	"fmt" // 导入 fmt 包，用于格式化输入输出
+)
+
+func main() { // 定义 main 函数，程序的入口点
+    fmt.Println("hello world") // 调用 fmt.Println 函数打印 "hello world" 到控制台
+}
